3952c0abd16db11812e22ab619b4fc53d99fa8f8
Josiah.wu <wuxingyu090@163.com>
2024-07-29 15:39:40 +0800

    1

--- src/data_interaction/os_path.py
+++ src/data_interaction/os_path.py
@@ -1,8 +1,9 @@
 import os
-from pathlib import Path, PurePosixPath
+from pathlib import Path
 
 # 获取当前脚本文件所在的目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
+
 
 class OsPathClass:
     def __init__(self, input_file=None, output_file=None, data_file=None):
@@ -18,7 +19,6 @@ class OsPathClass:
             return input_path
         else:
             raise FileNotFoundError("input_file can't None!")
-
 
     def output_path(self):
         if self.output_file is not None:
